a5fd5408754bfcd6ca7cbedd868a615382a1b939
RovaRakotoarisoa <rovarakotoarisoa114@gmail.com>
2025-05-03 22:23:01 +0300

    design navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 function Navbar(props) {
 	return (
-		<nav className="navbar navbar-expend-lg">
-			<div className="">
-				TExt
+		<nav className="bg-yellow-300/50 flex justify-center items-center w-full">
+			<div className="bg-white px-6 py-4 mt-20 rounded-[50px] flex gap-[24rem] justify-between items-center fixed shadow-lg backdrop:backdrop-blur-lg">
+				<div className="navbar__logo text-2xl font-bold">
+					<a href="#">ROVA</a>
+				</div>
+				<div className="navbar__link flex gap-6">
+					<a href="#" className="flex justify-center"><div className="bg-red-400/20 rounded-2xl w-[0.5rem] h-[0.5rem]"></div>About</a>
+					<a href="#" className="flex justify-center"><div className="bg-green-400/20 rounded-2xl w-[0.5rem] h-[0.5rem]"></div>Work</a>
+					<a href="#" className="flex justify-center"><div className="bg-blue-400/20 rounded-2xl w-[0.5rem] h-[0.5rem]"></div>Services</a>
+					<a href="#"className="flex justify-center"><div className="bg-purple-400/20 rounded-2xl w-[0.5rem] h-[0.5rem]"></div>Contact</a>
+				</div>
 			</div>
 		</nav>
 	);
 }
+
+export default Navbar;
